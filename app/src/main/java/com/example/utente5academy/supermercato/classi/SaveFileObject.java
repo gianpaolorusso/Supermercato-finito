1b7a9a5c63259a068c48104191177d593218231c
gianpaolo <gian962001@gmail.com>
2017-12-04 22:16:06 +0100

    sistemato layout per dispositivi piu obsoleti

--- app/src/main/java/com/example/utente5academy/supermercato/classi/SaveFileObject.java
+++ app/src/main/java/com/example/utente5academy/supermercato/classi/SaveFileObject.java
@@ -1,29 +1,17 @@
 package com.example.utente5academy.supermercato.classi;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
-import java.nio.file.DirectoryIteratorException;
-import java.util.ArrayList;
-import java.util.Iterator;
-
-import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by utente on 02/12/17.
